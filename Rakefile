446d16e4c266f827de1e3e41c08beee42340ef3c
Scott M Parrish <anithri@gmail.com>
2017-05-29 20:20:14 -0600

    getting ready for trials

--- Rakefile
+++ Rakefile
@@ -7,20 +7,26 @@ EXCLUDED_SRC_FILES = ['_skins-pseudo.css', '_styles.css', '_module-template.css'
                       '_links.css', '_lists.css', '_overflow.css',
                       '_tables.css', '_utilities.css', '_hovers.css',
                       '_debug-children.css', '_debug-grid.css', '_code.css',
-                      '_debug.css','_normalize.css', '_images.css',
-                      '_variables.css'
+                      '_debug.css', '_normalize.css', '_images.css',
+                      '_variables.css', '_font-family.css'
 ].map {|f| '**/' + f}
 
 TACHYONS_DIR            = Pathname('./node_modules/tachyons-custom/src/').expand_path
 RULES_DIR               = Pathname('./rules/').expand_path
 TEMPLATE_DIR            = Pathname('./templates/').expand_path
+MODULES_NONE            = TEMPLATE_DIR + 'modules'
+MODULES_SIDE            = TEMPLATE_DIR + 'modules-side-comments'
+MODULES_TOP             = TEMPLATE_DIR + 'modules-top-comments'
 STYLESHEET_GLOB_PATTERN = '_*.css'
 RULES_GLOB_PATTERN      = '*.yml'
 
 directory RULES_DIR
 directory TEMPLATE_DIR
+directory MODULES_NONE
+directory MODULES_SIDE
+directory MODULES_TOP
 
-CLOBBER.include(RULES_DIR.to_s + '/*', TEMPLATE_DIR.to_s + '/*')
+CLOBBER.include(RULES_DIR.to_s + '/*', TEMPLATE_DIR.to_s + '/**/*.xml')
 task :check_install do
   unless TACHYONS_DIR.exist?
     abort("#{TACHYONS_DIR} does not exist.\nPlease run yarn to install")
@@ -38,10 +44,20 @@ end
 
 files_to_generate = Rake::FileList[RULES_DIR.to_s + '/'+ RULES_GLOB_PATTERN]
 desc 'convert tachyons rules to xml'
-task generate: [TEMPLATE_DIR, *files_to_generate] do
-  s = Tach::GenerateTemplate.combine('tach-all', files_to_generate, comments: :none)
-  s.to_xml
-  s.save(TEMPLATE_DIR)
+task generate: [RULES_DIR, TEMPLATE_DIR, MODULES_NONE,
+                MODULES_SIDE, MODULES_TOP, *files_to_generate] do
+  t = Tach::GenerateTemplate.combine('tachyons', files_to_generate)
+  t.save(TEMPLATE_DIR, :side)
+  t.save(MODULES_NONE, :none)
+  t.save(MODULES_SIDE, :side)
+  t.save(MODULES_TOP, :top)
+
+  files_to_generate.each do |file|
+    t = Tach::GenerateTemplate.generate(file, prefix: 'tach-')
+    t.save(MODULES_NONE, :none)
+    t.save(MODULES_SIDE, :side)
+    t.save(MODULES_TOP, :top)
+  end
 end
 
 
--- lib/tach/generate_template.rb
+++ lib/tach/generate_template.rb
@@ -1,55 +1,46 @@
 require 'yaml'
 require 'nokogiri'
-
+require 'pry'
 module Tach
   class GenerateTemplate
     EXPECTED_FILE_EXT = '.yml'
-    COMMENT_TYPES     = [:name, :short, :verbose]
-
     # @param file [String,Pathname] name of file to read
-    # @param destination_dir [String, Pathname] dir to save templates to
-    # @param comments [Symbol, String] commenting style for templates.
-    #   one of [:none, :short, :verbose]
     # @param name [String] section name.  defaults to file.basename
-    def self.generate(file, destination_dir, comments: :short, name: nil)
-      section  = name || Pathname(file).basename(EXPECTED_FILE_EXT).to_s
-      comments = comments.intern
-      rules           = YAML.load_file(file)
-      s               = self.new(name, rules, comments)
+    # @param prefix [String] default: '' is prepended to name if no name is given
+    def self.generate(file, name: nil, prefix: '')
+      name  = name || (prefix + Pathname(file).basename(EXPECTED_FILE_EXT).to_s)
+      rules = YAML.load_file(file)
+      self.new(name, rules)
     end
 
     # @param name [String] name.  also name output filename
     # @param files Array[String,Pathname] name of files to read
     # @param destination_dir [String, Pathname] dir to save templates to
-    # @param comments [Symbol, String] commenting style for templates.
-    #   one of [:none, :short, :verbose]
-    def self.combine(name, files, destination_dir, comments: :short)
-      generator = self.new(name, [], comments)
+    def self.combine(name, files)
+      generator = self.new(name, [])
       Array(files).each do |file|
         generator.rules.concat(YAML.load_file(file).to_a)
       end
       generator
     end
 
-    attr_reader :name, :rules, :prefix
+    attr_accessor :name, :rules, :prefix
 
     # @param name [String] name of this group.  Also the filename it's saved as
     # @param rules Array[Hash[String,Hash]], list of rules as [selector, data] pairs
     #   as read from file
-    # @param comments [Symbol] commenting style for templates.
-    #   one of [:none, :short, :verbose]
-    def initialize(name, rules, comments)
-      @name     = name
-      @rules    = rules
-      @comments = comments
+    def initialize(name, rules)
+      @name  = name
+      @rules = rules
     end
 
-    def to_xml
-      @builder ||= Nokogiri::XML::Builder.new do |xml|
+    # @param comment_type [Symbol] default: :none commenting style for templates.
+    #   one of [:none, :side, :top]
+    def to_xml(comment_type = :none)
+      builder ||= Nokogiri::XML::Builder.new do |xml|
         xml.templateSet(group: name) do
           self.rules.each do |(rule_name, data)|
-            values = data['properties'].join("\n") + "\n$END$"
-            attrs  = attrs(rule_name, values)
+            attrs = attrs(rule_name, data['properties'], comment_type)
             xml.template(attrs) do
               xml.method_missing('context') do
                 xml.option(name: 'CSS_DECLARATION_BLOCK', value: true)
@@ -58,36 +49,52 @@ module Tach
           end
         end
       end
-      @builder.to_xml
+      builder.to_xml
     end
 
     # the file must be saved with the same name as the group attribute on templateSet
     #   for this reason, we get the dir not the path
-    def save(directory)
-      if ! rules.empty?
-        File.open(directory + filename, 'w') {|f| f.puts self.to_xml}
+    # @param directory [String,Pathname] directory to save file to
+    # @param comment_type [Symbol] default: :none commenting style for templates.
+    #   one of [:none, :side, :top]
+    def save(directory, comment_type = :none)
+      if !rules.empty?
+        File.open(directory + filename, 'w') {|f| f.puts self.to_xml(comment_type)}
       else
         puts "No rules for #{name}"
       end
     end
 
-
     def filename
       name + ".xml"
     end
 
-    def attrs(rule_name, values)
+    def attrs(rule_name, properties, comment_type)
       {
           name:             rule_name,
-          value:            comments(rule_name) + values,
-          description:      comments(rule_name, comments: true),
+          value:            with_comments(rule_name, properties, comment_type),
+          description:      short_comment(rule_name),
           toReformat:       false,
           toShortenFQNames: true
       }
     end
 
-    def comments(rule_name, comments: @comments)
-      @comments ? "/* #{name}: #{rule_name} */\n" : ''
+    # TODO REFACTOR if this gets anymore complicated
+    def with_comments(rule_name, properties, comment_type)
+      props = properties + ["$END$"]
+      case comment_type
+        when :side # first line gets the comment appended to it
+          props[0] += " /* #{short_comment(rule_name)} */"
+        when :top # comment is added as first line
+          props.unshift "*/ #{short_comment(name, rule_name)} */"
+        else # mostly :none
+          # noop
+      end
+      props.join("\n")
+    end
+
+    def short_comment(*parts)
+      parts.join(": ")
     end
   end
 end
